a962064a10f9b76876572c7dcd387cc510037c4c
FrankR27 <frankdejesusramosguzman9@gmail.com>
2022-08-09 15:08:17 -0400

    Add haroku needed config

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 4001;
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
